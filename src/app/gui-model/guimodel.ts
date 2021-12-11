c247c3fceea627cc935f09cf5887afd407064b13
lourusti <rustelou@students.zhaw.ch>
2021-12-11 14:34:41 +0000

    Name Aenderung

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Freunde Lou",
             "formList": [
                 {
                     "id": "FriendForm",
